a89452c852cdc125932e1280cb20c62604603d8c
e_yatsevich <katy.yats23@gmail.com>
2019-09-23 16:15:46 +0200

    feat: add brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0) return false;
+
+  let leftChecked, rightChecked;
+
+  for (let i = 0; i < str.length; ) {
+    let state = false;
+
+    leftChecked = str[i];
+    rightChecked = str[i+1];
+
+    for (let j = 0; j < bracketsConfig.length; j++) {
+      if (leftChecked !== bracketsConfig[j][0]) continue;
+      if (rightChecked !== bracketsConfig[j][1]) {
+        state = false;
+      } else {
+        state = true;
+        str = str.slice(0, i).concat(str.slice(i+2));
+      }
+    }
+    if (state) {
+      i = (i === 0) ? i = 0 : i -= 1;
+    } else {
+      i++; 
+    }
+  }
+  if (str.length === 0) {
+    return true;
+  } else {
+    return false; 
+  }
 }
